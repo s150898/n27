60c0807f69208d9fd213c2d8e382495c4a451e92
s150898 <s150898@berufskolleg-borken.de>
2019-03-20 10:58:43 +0100

    ...

---
+++ Training/if-else-training.js
@@ -0,0 +1,61 @@
+/**
+ * Hier können Sie IF und ELSE trainieren.
+ * Am besten dazu den nodemon starten
+ * 
+ *  .\node_modules\.bin\nodemon Training\if-else-Training.js
+ * 
+ * und die Ausgabe im Terminal beobachten.
+*/
+
+
+// Die Zahl 1 wird zugewisesn an eine Variable namens x
+let x = undefined
+let y = 100
+
+if("GW11b".includes("11"))if("GW11b".includes("11")){
+    console.log("Die Prüfung ist WAHR. Es werden die Anweisungen im Rumpf von IF ausgeführt.")
+}else{
+    console.log("Die Prüfung ist FALSCH. Es werden die Anweisungen im Rumpf von ELSE ausgeführt.")
+}
+
+// 21. if("GW11b".includes("11"))if("GW11b".includes("11")){ 
+
+// 20. if("GW11b".includes("11")) Die Prüfung ist wahr. Die Methode includes prüft, ob ein String einen anderen String enthält.
+
+// 19. if("GW11b".endsWith("b")) Die Prüfung ist wahr. Die Methode endsWith prüft, ob ein String mit einem anderen String endet.
+
+// 18. if("GW11b".startsWith("GT")) Die Prüfung ist falsch. Die Methode startsWith prüft, ob ein String mit einem anderen String beginnt.
+
+// 17. if("Max".length) Die Prüfung ist wahr. Der Werten von ("Max".length) ist 3.
+
+// 16. if(undefined) Die Prüfung ist falsch. Wenn ein objekt nicht existiert, ist es "undefined".
+
+// 15. if(0 > x || x < 10) Logisches Oder. Die Prüfung ist wahr, wenn die linke oder die rechte oder beide Prüfungen wahr sind.
+
+// 14. if(x > 0 && x < 11 ) Logisches Und. Die Prüfung ist wahr, wenn beide Prüfungen wahr sind.
+
+// 13. if("") Die Prüfung ist falsch.
+
+// 12. if("#108$") Die Prüfung ist wahr. Alle nicht leeren Zeichenketten (Strings) sind wahr.
+
+// 11. if(123) Die Prüfung ist wahr. Alle Zahlen außer 0 sind wahr.
+
+// 10. if(0) Die Prüfung ist falsch.
+
+// 9. if(false) Die Prüfung ist falsch. Der Rumpf von IF ist unereichbarer Code.
+
+// 8. if(true) Der Ausdruck ist wahr. Der Rumpf von ELSE ist unereichbarer Code.
+
+// 7. if(1 === "1") Prüfung auf Gleichheit des Wertes und Gleichheit des Typs. Die Prüfung ist falsch.
+
+// 6. if(0 != 1) Prüfung auf Ungleichheit. Die Prüfung ist wahr.
+
+// 5. if(0 <= 1) Prüfung "kleiner gleich". Die Prügung ist falsch.
+
+// 4. if(0 >=1 ) Prüfung "größer gleich". Die Prüfung ist falsch. 
+
+// 3. if(0 < 1) Prüfung auf "kleiner als". Die Prüfung ist wahr. 
+
+// 2. if(0 > 1) Prüfung auf "größer als". Die Prüfung ist falsch.
+
+// 1. if(1 == 1) Prüfung auf Gleichheit des Wertes. Die Prüfung ist wahr.
